c531cf9e52377a2849ba1de077dca5038ec92082
Martian238 <67149417+Martian238@users.noreply.github.com>
2026-05-25 21:56:41 +0800

    swswswjswj

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,7 @@ Vars.renderer.maxZoom = 25;
 Vars.renderer.minZoom = 0.2;
 Vars.appName = "Mindustry: Vanilla Expansion";
 //require("xfkjqjs");
+/*
 Team.green.color.set(Color.valueOf("00ffce"));
 Team.green.palette[0].set(Color.valueOf("7effea"));
 Team.green.palette[1].set(Color.valueOf("3ad2b5"));
@@ -16,6 +17,13 @@ Team.green.palette[2].set(Color.valueOf("189886"));
 Team.green.hasPalette = true;
 Team.green.ignoreUnitCap = true;
 Team.green.name = "lacuna";
+*/
 Planets.tantros.visible = true;
 require("blocks");
+log("endblocks");
 require("sectors");
+log("endsectors");
+require("team2");
+log("endteam2");
+//require("planets");
+log("endplanets");
