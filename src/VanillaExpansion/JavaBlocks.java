ec21960416f5c5d9607aeb70bb67a194ba0eeebd
Martian238 <67149417+Martian238@users.noreply.github.com>
2026-07-13 15:26:03 +0800

    final collapse

--- src/VanillaExpansion/JavaBlocks.java
+++ src/VanillaExpansion/JavaBlocks.java
@@ -1,23 +1,31 @@
 package VanillaExpansion;
 
 import VanillaExpansion.MultiCrafter;
+import arc.struct.Seq;
 import mindustry.world.Block;
 
 public class JavaBlocks {
     public static Block
-    fluidSorter, cargoAnchor,
+ /*   fluidSorter, cargoAnchor,
     boiler, heatTurbineGenerator,
-    reductionChamber, upgradedReactionPool, configMelter
+    reductionChamber, upgradedReactionPool, configMelter*/
+    testJavaBlock
             ;
 
     public static void load(){
 
-        fluidSorter = new MultiCrafter("m-fluid-sorter");
+        /*fluidSorter = new MultiCrafter("m-fluid-sorter");
         cargoAnchor = new MultiCrafter("cargo-anchor");
         boiler = new MultiCrafter("m-boiler");
         heatTurbineGenerator = new MultiCrafter("heat-turbine-generator");
         reductionChamber = new MultiCrafter("m-reduction-chamber");
         upgradedReactionPool = new MultiCrafter("m-upgraded-reaction-pool");
-        configMelter = new MultiCrafter("config-melter");
+        configMelter = new MultiCrafter("config-melter");*/
+        testJavaBlock = new MultiCrafter("test-java-block"){{
+            size = 2;
+            hasItems = true;
+            baseExplosiveness = 99f;
+            category = category.crafting;
+        }};
     }
 }
